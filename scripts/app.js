1b5fed89c188853cb5a36c1ff2c2d6118fc58d98
Fahri MacBook <fahri-can@web.de>
2018-05-18 12:47:09 +0200

    Test controller scope.

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,19 @@
 angular.module('todoListApp', [])
-    .controller('mainCtrl', function($scope) {
+    .controller('mainCtrl', function ($scope) {
         $scope.helloWorld = function () {
             console.log("This is the helloWorld() function!");
         };
+    })
+    .controller('coolCtrl', function ($scope) {
+        $scope.whoAmI = function () {
+            console.log("WHo am I?");
+        },
+        $scope.helloWorld = function () {
+            console.log("coolCtrl helloWorld() function!");
+        };
+    })
+    .controller('siblingCtrl', function ($scope) {
+        $scope.siblingTest = function () {
+            console.log("I am a sibling.")
+        }
     });
